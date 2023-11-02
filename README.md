7a88729ffcc179aa6c26abd5c539e42b21c2aebe
Will <willcburroughs@gmail.com>
2023-11-02 22:43:07 +0000

    Basic functionality complete

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # **SQL HEROES TEMPLATE** 
 
-## Setup
-- After creating your own repo from the template, create a local copy either with the Github CLI tool (recommended) or a git clone.
-- Make sure you have Docker Desktop downloaded and running
-- Open VScode and install the [Dev Containers Extension](https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-containers)
-- Open your repo in VScode, after a few moments you should see a prompt from VScode asking if you want to reopen the repo in a dev container, follow the prompt to do so. If you are not prompted, you can click the remote connection icon in the bottom left corner and select 'Reopen in Container'
-- Once the container is built, click the SQL tools extension to establish your database connection. All required fields, including password, are 'postgres'.
-- Proceed to set up your file structure per Project document.
+## Basic project 
+- Basic project must include crud functionality (Create, read, update, delete)
+- I create heros through use of my create Hero function 
+- I read by rendering the current table to the user by use of my renderAll function 
+- I update values within the table through use of my updateHero function 
+- I delete values in my table through use of my updateHero function 
+- The basic project now works (Done)
+
+## Refactor Basic 
+- Go back through and have input statements to make this more user friendly 
+- Handle for more error cases and continue so that this will run whenever user needs functionality 
+
+
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import time
 from database.db_connection import execute_query, execute_modify
 
 
@@ -36,14 +37,6 @@ def create_hero(hero):
 # Adding new ability
 Luffy = Hero("Luffy", "King of the pirates", "He is Dylan")
 
-# Insert new hero
-create_hero(Luffy)
-
-# Deleting 
-delete_by_Value("Luffy")
-
-create_hero(Luffy)
-
 # Update hero property (Update)
 def updateHero(name, property, newValue):
     check_query = "SELECT id FROM heroes WHERE name = %s;"
@@ -70,7 +63,109 @@ def render_all():
     for row in rows:
         print(row)
 
-# Call the render
-render_all()
+# Allows for seeing names of characters 
+def render_names(): 
+    select_query = "SELECT * FROM heroes"
+    rows = execute_query(select_query)
+    for row in rows:
+        print(row[1])
 
 
+
+user_continue = True
+
+while(user_continue): 
+
+    user_Actions = ["create", "read", "update", "delete", "rick"]
+
+    userChoice = input("Hello What Would you like to do?: (*Create, Read, Update, Delete, Rick*) ").lower()
+
+    if(userChoice not in user_Actions): 
+        print("That is not a supported action. Supported actions are: create, read, update, delete, and rick")
+
+    elif(userChoice == "update"): 
+        see_list = input("Would you like to see the current list of heroes? (y/n) ").lower()
+
+        if(see_list == "y"):
+            render_names()
+        
+        possible_properties = ["biography", "name", "about_me"]
+        selected_character = input("What character would you like to update? ")
+        selected_property = input("What property would you like to change(biography, name, about_me) ")
+
+        while(selected_property not in possible_properties):
+            print("Selected property not available")
+            selected_property = input("What property would you like to change(biography, name, about_me) ")
+
+
+
+    elif(userChoice == "create"):
+        
+        finished_Character = False
+
+        while(not finished_Character):
+            name = input("What is your name? ")
+            about_me = input("Can you give us a quick description of yourself? ")
+            biography = input("Can you give us your background? ")
+            new_Hero = Hero(name, about_me, biography)
+            create_hero(new_Hero)
+            done = input(f"""
+Is this how you would like your character (y/n)?  
+Name: {name}
+About Me: {about_me}
+Biography: {biography}
+""")
+            if(done == "y"):
+                finished_Character = True
+                print("Character created and added")
+        
+
+    elif userChoice == "rick":
+        print(
+            """
+00000000000KKKKK0KKKXKK0OOOOOOO0KXXXKK0000O00OkkkkOOKKXKXXXXKKK00OkxkkxkkkO00KKKKKKK000000000OOOOOkx
+000000KKK0KKKKK00000000O000O0000KK000OkkkkkkkkkkkkkkkkkO00000KKKK0Okkkxkkkk0KKKKKKKKK000000000000Okk
+000KXXXXK0KNNXXKKKK00000000000000OOkkkOOOkkkkkdcc::::;;:loxOOOOOOOOkxxkkkkkkOOO00000000000000000OOkk
+KKKXXXK000XNXXXXXXXXXXXXK0000000KK000OOOOOkkkdc,''''',,',;:okkkkkkkxxxkkkkkkkO0000KXXXXKKXXXK00OOkkk
+XXXXKK00KKXNXXXXXXXXXNNXKK00000KKXXXKK0OOOOOxo:;;;;,,;;;,,,;oOK00OkkkkkkkO000KXXXXXNNNNKKKXNXKOkkkkk
+XXXXKKKKXXNNXXXNNXXXKKKKK0000KKKKK0000OOOOOolooooddddddddl;,lO00OOkkkkkkkk0KKKKKXXXXNNXK0000000OOkkk
+0KKK00KXNNNNXXKKKK0000KKXK000KKK0000000OOkxlldolooddxxxxkdc;okkkxkkOOkxkkkkkOO00KXXXNNNXKK0OOO0KK0OO
+0000KXNNNNNXK00000000KXXNK0000K000KXXXXXKOkxdoc:cllodoooddc:x0OkkkkOOkxkkOOkxkkk000KXXNNXK0OkOO00OOO
+K000XNNNNXK000KXXK000KNNNXK000KKXXNNNXXNXOxkxollccloxdooddlxKKKKOkkkkkkkk000OkkkkkkO0KXXXK0OkkOOOO0K
+XK00KKKK00000KXXNNXXKKXNNXKKKKKXNNNNNXXXXOdkkolllllodxxxxddkKKKK00Okkkkkk0K0OkkkO0OOkOO0KKKKOOOO00KN
+X0000000KK000KXXNXKKKKKKKKKKK0KXNNNNNXXXXKOOkollcloodxxxxxk0KKKKKK0kkkkkk00OkO000K0OkkkkOO0K0OO0KXNN
+X000KK00KXXK000KXK00KXXXKK0KK0KNWWNNNXXXXKKKkollcllodddxkO0KKKKKK00kxkkkkkkOkkO00KK0OkkkkkkOOO0KXNNN
+X00KXXK00KK00KK0000XNNNNXK00000KXXNNNXXXK0OOxolllooddddxO0KK0K0K000OkxxkkkO00OkO0K0OkkO00OkO0000XNWW
+000KNNXK0000KXXXK00XNNNNX0000KK00KKXXXX0OkkkdllccllooddxxxO0KKK00kkkkkkxxO0KKK0OOOOOOkO0OOOKXK0KXNWW
+0000KKKKK0O0KNNNNK0KNNNNK000KXXK0OO00OOOkOOxdlllodddddxxc,cdkOOOOkkOOkkkkO0KKXKO0KXK0OkkkO0XNK00KXNN
+OO00000OOOOO0KXXXX00XXXXK00000000000OkxxkOkdooollooodxOd;...';:clodxxkkkkk00KK0OKXXKKOkkO0XXXXK000KK
+00KKKK0OO0K0OOKXXX00KXXK0OOOOOO000Oxoc:cxOkdooollodxk0kc............';:coxO000OOKXXK0kkkkkOOOO0OO000
+XXXXXK0O0XXK0OOKXK0OO000OkkOOkxdlc;'...;dkkoccldxOOOOx:..................;x00OkOKKK0kkOkkkO0000000KK
+XXXXK0OO0XKK0OO0000000OOOkkkkl,'.......':cllcclddxxxo;....................lOOkkk00OkkO00OkkOKKKKKKXX
+XXXXKOOOOOOOOOkkOOKXXXX0OOkkx;............:c::lollll;.....................,okOOkkkkxkO0K0kxk0KKKKKXX
+XKKK0OkOO00OOkkkkOKXXXNKOOOkd,............;cllcclcc:'......................;k0OOkkxxxkkOOOkkOKKKKKXX
+0OOkkOO0KK00OkkkkO0KXKXKOkkkd,............;cddc:c::,.......................,dO0OkkxxkO00OkkkO00KKKKK
+OkkO0KKKKK0Okk0OkkO0KXXKOkkko,............;lxdc::;,.........................,dOOkkkkkOKXK0OOkkkOOOOO
+0Ok0KKKKK0OkkOK0OkkOKXX0OOkkl.............;lddc::,...................   .....,coxkO0kOKXXXXK0Okkkkkk
+0OkO0KKKK0OkO0K00OOO0KX0OOkkl.............,:c:::;'......,:::;;;,.....  .........,lk0OO0XXXNNXKOOO00O
+0kkkk0KKK0kk0K0OOO00O0K0Okkkl'............';:::;,.......;cclloolc'...   .  ........:okO0XXXXXKOO0K00
+OxkkkO000OkO00kkkk0KOO00kkkkd'... .........,::;;'........,:clllloc'..... ...........'okO0KXK0OkO0K00
+kkOOkkxkkxxkkxxOOOOOOOOOkkxko'...  ........,;;;,..........,:cllloo:..................lkkOKK0OkkOO000
+O0000kxxxxxxxxO00KKOkkkkkkxxl'...   .......,;;,'.......  .....';cl:'.................:xxkOOkOOOOkkkk
+OOOO0OkxddxxkO000KKK0Okkkxxxc'.....',,,,''.,;;,'.   ..         ..... ..''............,oxxxxk0000Oxdd
+kkkkkkkxddxkkkOO000000Okkxxxo;..':clllc:;..,;,,..                    ...'...........,ldddxxkOOOOOkxd
+OOOkkOkxdddxkkOO00000Okkxxxxd,.',,::;;;;,..';,'.                       .,''..'',,;coxxddddxxkkkkkkxd
+kkkkkkkdoooodxkOO0Okkxxxxxdxdl;'..';;;;;'..',,'.           ...         ;dddddddxxkkkxxdoodxkkkkkkkxx
+kkkkkkxdooddodxkOkxxxxxxxxddxdl:;...,,,'. .'',..                       'oxxxddddxxxxdooooodxxxxxxkxx
+xxxxxxxooodxdooddddxkkxdddddxxdooc,'....   .''.                   ......cddddddddddddddooodxxxxxxxxd
+xxxxxxdooodxxddodxkkOkxdddddxkkxdolol;......'..                  .......:odddddddddoddddooddxddxdxxd
+ddddddolloddddoodxkkkxddddddxkkxdolol,....,;,'.                   ......'ldodddddddddddooooddddddddd
+ooooooolloddooooodxkkxdddooodxxdolll:'....,;,'..                  .......:ooodoooooooddooooodddddddd
+            """)
+    elif userChoice == "read":
+        render_all()
+        continue_input = input("Would you like to continue?")
+    
+    
+
+# user_continue = False÷
